c5323b06b0c6ece844cdd661eb5b7b077e087810
Maxigameshd22 <Maxigameshd22@gmail.com>
2025-01-05 19:52:38 +0100

    Delete kubejs directory

--- kubejs/startup_scripts/Custom/garnet_gross.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('item', event => {
-    event.create('garnet_gross')
-    .maxStackSize(1)
-    .displayName('§1Garnet Gross')
-    .tooltip("Click for the Ultimate Ability")
-    .texture('layer0','kubejs:item/garnet_gross')
-})
